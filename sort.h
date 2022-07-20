0ab2b69127aa21eece0b26e92f154354273b3008
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-07-20 03:11:23 +0100

    O-bubble_sort and 0-O

--- sort.h
+++ sort.h
@@ -1,9 +1,9 @@
-#ifndef _SORT_H_
-#define _SORT_H_
+#ifndef _SORT_H
+#define _SORT_H
+
+#include <stdlib.h>
 #include <stdio.h>
-
-
-/** Struct Double Linked List */
+#include <time.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -14,22 +14,30 @@
  */
 typedef struct listint_s
 {
-	const int n;
+	int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
 
-/** Given FUN prototype */
 
+/* print.c */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void check_list(const listint_t *list);
+void check_array(const int *array, size_t size);
+int *rand_array(int len, int max);
 
-/** TASK Prototype */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
 #endif
